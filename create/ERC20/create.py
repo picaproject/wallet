b16cba771c82d903626cb97bed1359b82124a1cc
PICA PROJECT <72856240+picaproject@users.noreply.github.com>
2021-06-14 15:25:51 +0900

    create erc20 wallet

--- create/ERC20/create.py
+++ create/ERC20/create.py
@@ -1,2 +1,8 @@
+from web3 import Web3, HTTPProvider
+
+
 def create_wallet():
-    pass
+    ethUrl = ""
+    web3 = Web3(HTTPProvider(ethUrl))
+    password = ""
+    ethAddress = web3.personal.newAccount(password)

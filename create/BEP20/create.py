cb0b42dc2f931aaf07c640aa3ea422bf01386f15
PICA PROJECT <72856240+picaproject@users.noreply.github.com>
2021-06-14 15:24:45 +0900

    create bep20 wallet

--- create/BEP20/create.py
+++ create/BEP20/create.py
@@ -1,2 +1,14 @@
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
+
+
 def create_wallet():
-    pass
+    bscUrl = ""
+    web3 = Web3(HTTPProvider(bscUrl))
+    web3.middleware_stack.inject(geth_poa_middleware, layer=0)
+
+    password = ""
+    bscAccount = web3.eth.account.create(password)
+
+    bscAddress = bscAccount.address
+    privateKey = bscAccount.privateKey
